b45d3d50dee37eeb3c66277a881f64b29a664370
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-03-17 22:20:24 +0200

    Add JS Syntax Fundamentals - Exercise

---
+++ 01. JS Syntax Fundamentals - Exercise/07.Chars to String.js
@@ -0,0 +1,16 @@
+function charsToString(char1, char2, char3) {
+    console.log(char1 + char2 + char3);
+}
+
+charsToStr('a',
+    'b',
+    'c'
+)
+charsToStr('%',
+    '2',
+    'o'
+)
+charsToStr('1',
+    '5',
+    'p'
+)
