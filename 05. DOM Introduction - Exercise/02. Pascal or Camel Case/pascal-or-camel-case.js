e6977f2d2d0eba6dc9aea9576937729ed1a8b1ad
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-04-04 20:04:14 +0300

    Add DOM Introduction - Exercise

---
+++ 05. DOM Introduction - Exercise/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -0,0 +1,26 @@
+function solve() {
+    let textInput = document.getElementById('text');
+    let commandInput = document.getElementById('naming-convention');
+
+    let text = textInput.value.toLowerCase();
+    let tokens = text.split(' ');
+    let command = commandInput.value;
+
+    let output = document.getElementById('result');
+
+    if (command != 'Pascal Case' && command != 'Camel Case') {
+        output.textContent = 'Error!';
+        return;
+    }
+
+    let startIndex = command == 'Pascal Case' ? 0 : 1;
+
+    for (let i = startIndex; i < tokens.length; i++) {
+        let word = tokens[i];
+        word = word[0].toUpperCase() + word.slice(1);
+        tokens[i] = word;
+    }
+
+
+    output.textContent = tokens.join('');
+}
